a9a965d0297b7aaec28591a1a20ce863120e027a
Luigi160397 <luigi.difraia.97@gmail.com>
2023-04-07 13:16:19 +0200

    fetch

--- src/components/Meteo.jsx
+++ src/components/Meteo.jsx
@@ -3,8 +3,35 @@ import ButtonHome from "./ButtonHome";
 import MainCard from "./MainCard";
 import Forecast from "./Forecast";
 import InfoMeteo from "./InfoMeteo";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const Meteo = () => {
+  const lat = "41.8933203";
+  const lon = "12.4829321";
+
+  const endpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=it&units=metric&appid=7a8ddce8f84f177f053d45de79e6ca77`;
+
+  const meteo = useSelector(state => state.meteo.content);
+  const dispatch = useDispatch();
+
+  const request = async endpoint => {
+    try {
+      const response = await fetch(endpoint);
+      if (response.ok) {
+        const data = await response.json();
+        dispatch({ type: "CURRENT_METEO", payload: data });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    request(endpoint);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Container fluid>
       <ButtonHome />
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,19 @@
-const initialState = {};
+const initialState = {
+  meteo: {
+    content: []
+  }
+};
 
 const mainReducer = (state = initialState, action) => {
   switch (action.type) {
+    case "CURRENT_METEO":
+      return {
+        ...state,
+        meteo: {
+          ...state.meteo,
+          content: [action.payload]
+        }
+      };
     default:
       return state;
   }
